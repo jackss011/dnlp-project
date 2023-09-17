41f28b5a01eebe3a69b247bfcba6e7c727cc4c0a
Jackss <jackss.code@gmail.com>
2023-09-17 12:43:58 +0200

    Added pca run results

--- scidocs/run-evaluate-pca.sh
+++ scidocs/run-evaluate-pca.sh
@@ -1,3 +1,5 @@
-for d in "pca-embeddings/*/" ; do
-    echo "$d"
+for d in pca-embeddings/*/ ; do
+    nc=$(basename "$d")
+    #./run-evaluate.sh pca-embeddings/nc100 pca-results/nc100.csv
+    ./run-evaluate.sh $d "pca-results/$nc.csv"
 done
