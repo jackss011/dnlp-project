dab71fa60d9f067e3aa0c90f0cfe229e70154b72
Jackss <jackss.code@gmail.com>
2023-09-17 21:49:34 +0200

    fixed script pca

--- scidocs/run-evaluate-pca.sh
+++ scidocs/run-evaluate-pca.sh
@@ -1,4 +1,4 @@
-for d in pca-embeddings/2/*/ ; do
+for d in pca-embeddings/*/ ; do
     nc=$(basename "$d")
     #./run-evaluate.sh pca-embeddings/nc100 pca-results/nc100.csv
     ./run-evaluate.sh $d "pca-results/$nc.csv"
